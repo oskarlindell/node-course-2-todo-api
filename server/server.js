3839a4fba99ea7c57fcd0b4c7c1f8ff2b69039a1
Oskar Lindell <oskar@oskarlindell.com>
2020-03-21 14:42:59 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
@@ -24,15 +25,32 @@ app.post("/todos", (req, res) => {
   );
 });
 
-app.get("/todos", (req, res) => {
-  Todo.find().then(
-    todos => {
-      res.send({ todos });
-    },
-    e => {
-      res.status(400).send(e);
-    }
-  );
+// app.get("/todos", (req, res) => {
+//   Todo.find().then(
+//     todos => {
+//       res.send({ todos });
+//     },
+//     e => {
+//       res.status(400).send(e);
+//     }
+//   );
+// });
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      return res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
