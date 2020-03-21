1f405a3c3977c9317c8ff926ad3275f1c0d59055
Oskar Lindell <oskar@oskarlindell.com>
2020-03-21 16:35:45 +0100

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -25,16 +25,16 @@ app.post("/todos", (req, res) => {
   );
 });
 
-// app.get("/todos", (req, res) => {
-//   Todo.find().then(
-//     todos => {
-//       res.send({ todos });
-//     },
-//     e => {
-//       res.status(400).send(e);
-//     }
-//   );
-// });
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
 
 app.get("/todos/:id", (req, res) => {
   var id = req.params.id;
